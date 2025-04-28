0966678ff1d4e46f9ec1b3d261805a18c4a2fef3
SP22-BCS-092 <SP22-BCS-092@cuilahore.edu.pk>
2025-04-28 13:58:11 +0500

    updated APIs and urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import signup, verify_email, verify_otp,resend_verification_email, forgot_password, reset_password
+from .views import signup,verify_signup_otp,resend_signup_verification_email, forgot_password,verify_forgot_password_otp, reset_password, resend_forgot_password_email,signin_view
 
 urlpatterns = [
     path('signup/', signup),
-    path('verify-email/', verify_email),
-    path('verify-otp/', verify_otp),
-    path('resend-verification-email/', resend_verification_email),
+    path('verify-signup-otp/', verify_signup_otp),
+    path('resend-signup-verification-email/', resend_signup_verification_email),
     path('forgot-password/', forgot_password),
+    path('resend-forgot-password-email/', resend_forgot_password_email),
+    path('verify-forgot-password-otp/', verify_forgot_password_otp),
     path('reset-password/', reset_password),
+    path('signin/', signin_view),
 ]
 
