4c742d0e3c1d744fda4c531cb1ceb303600d29d9
SP22-BCS-092 <SP22-BCS-092@cuilahore.edu.pk>
2025-05-17 11:56:12 +0500

    changed settings.py

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -128,16 +128,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 print("settings.py is running...")
 
 
-import dj_database_url
+# 
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.contrib.gis.db.backends.postgis',
             'NAME': 'urbananalytics',
